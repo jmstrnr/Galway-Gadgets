1ed46d907522f288b411927ed0428065e203ee33
jmstrnr <61889607+jmstrnr@users.noreply.github.com>
2021-05-12 13:35:52 +0100

    add login prompt

---
+++ retrieve.php
@@ -0,0 +1,60 @@
+<?php
+	
+  // The below code retrieves the contents of the products table from the g00398323 database (which can be imported into
+  // XAMPP with the provided g00398323.sql file), arranging the results in a HTML table.
+
+  //Step 1:  Create a database connection
+  $dbhost = "localhost";
+  $dbuser = "root";
+  $dbpassword = "";
+  $dbname = "g00398323";  // dbname is same as GMIT student id
+
+  $connection = mysqli_connect($dbhost,$dbuser,$dbpassword,$dbname);
+  
+  //Test if the connection occoured
+  if(mysqli_connect_errno()){
+    die("DB connection failed: " .
+      mysqli_connect_error() .
+        " (" . mysqli_connect_errno() . ")"
+        );
+  }
+
+  if (!$connection)
+    {
+      die('Could not connect: ' . mysqli_error());
+    }
+  
+  //Step 2: Perform the query to retrieve all data from the products table
+
+  $result = mysqli_query($connection,"SELECT * FROM products");  
+  
+  //Step 3: Return the database data and arrange it in a HTML table
+  echo "<table border='1' id='myTable'>
+  <tr>
+  <th>ID</th>
+  <th>Name</th>
+  <th>Price</th>
+  <th>Rating</th>
+  <th>Number of Reviews</th>
+  </tr>";
+  
+  while($row = mysqli_fetch_array($result))
+  {
+    echo "<tr>";
+    echo "<td>" . $row['id'] . "</td>";
+    echo "<td>" . $row['name'] . "</td>";
+    echo "<td>" . $row['price'] ."</td>";
+    echo "<td>" . $row['rating'] ."</td>";
+    echo "<td>" . $row['number_of_reviews'] ."</td>";
+    echo "</tr>";
+  }
+  
+  echo "</table>";
+  
+  // Step 4:  Release the returned data 
+  mysqli_free_result($result);
+  
+  // Step 5:  Close the database connection
+  mysqli_close($connection);
+  
+?>
